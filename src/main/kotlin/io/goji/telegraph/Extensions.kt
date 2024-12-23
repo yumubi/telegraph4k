9da2532c6f8d6dd6576a940e5e009faa9dc79275
yumubi <shaman2077c@163.com>
2024-12-23 13:05:36 +0800

    :egg: format

--- src/main/kotlin/io/goji/telegraph/Extensions.kt
+++ src/main/kotlin/io/goji/telegraph/Extensions.kt
@@ -2,11 +2,13 @@ package io.goji.io.goji.telegraph
 
 import io.goji.io.goji.telegraph.models.Node
 
+// Represents a DOM element node.
 fun NodeElement(
     tag: String,
     attrs: Map<String, String>? = null,
     children: List<Node>? = null
 ): Node = NodeElement(tag, attrs, children)
+
 
 fun String.toNode(): Node = this
 
